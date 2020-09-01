1c38aa095a6d99e4bf366a4c9f3e112aeb1370ab
Vilde Schulerud Bøe <vildesbo@uio.no>
2020-09-01 13:27:43 +0200

    Implement add

--- test_vector.py
+++ test_vector.py
@@ -7,3 +7,13 @@ def test_vector_str():
 def test_vector_repr():
     v = Vector3D(1, 2, 3)
     assert repr(v) == "Vector3D(1, 2, 3"
+
+
+def test_vector_add():
+    v = Vector3D(1, 2, 3)
+    u = Vector3D(1, 1, 1)
+    w = u+v
+    assert w.x == 2
+    assert w.y == 3
+    assert w.z == 4
+    assert w == Vector3D(2, 3, 4)
--- vector.py
+++ vector.py
@@ -10,6 +10,17 @@ class Vector3D:
     def __repr__(self):
         return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z}"
 
+    def __add__(self, other):
+        w = Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
+        return w
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
+
 if __name__ == "__main__":
-    v=Vector3D(1, 4, 1)
-    print([v])
+    v=Vector3D(1, 4, 2)
+    u = Vector3D(1, 1, 1)
+    w=v+u
+    w == Vector3D(2, 5, 4)
+    
