f937e67885df6d9e6f063a16cdce3cfc2cc7a1ae
Vilde Schulerud Bøe <vildesbo@uio.no>
2020-09-01 12:45:19 +0200

    AAdded repr in vector and test

--- test_vector.py
+++ test_vector.py
@@ -3,3 +3,7 @@ from vector import Vector3D
 def test_vector_str():
     v = Vector3D(1, 2, 3)
     assert str(v) == "(1, 2, 3"
+
+def test_vector_repr():
+    v = Vector3D(1, 2, 3)
+    assert repr(v) == "Vector3D(1, 2, 3"
--- vector.py
+++ vector.py
@@ -7,6 +7,9 @@ class Vector3D:
     def __str__(self):
         return f"({self.x}, {self.y}, {self.z}"
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z}"
+
 if __name__ == "__main__":
     v=Vector3D(1, 4, 1)
-    print(v)
+    print([v])
