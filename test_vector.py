3f93af6ce6a4435fa6e48838a4e64456f57f0ab5
Vilde Schulerud Bøe <vildesbo@uio.no>
2020-09-01 13:57:29 +0200

    Implement right addition, dot product

--- test_vector.py
+++ test_vector.py
@@ -13,7 +13,24 @@ def test_vector_add():
     v = Vector3D(1, 2, 3)
     u = Vector3D(1, 1, 1)
     w = u+v
-    assert w.x == 2
-    assert w.y == 3
-    assert w.z == 4
     assert w == Vector3D(2, 3, 4)
+
+def test_vector_add_integer_right():
+    v = Vector3D(1, 2, 3)
+    u = 1
+    w = u+v
+    assert w == Vector3D(2, 3, 4)
+
+import pytest
+def test_vector_add_str_raises_TypeError():
+    v = Vector3D(1, 2, 3)
+    u = "Hello"
+    with pytest.raises(TypeError):
+        u + v
+
+def test_vector_dot_product():
+    v = Vector3D(1, 2, 3)
+    u = Vector3D(1, 1, 1)
+    w = u.dot(v)
+    assert isinstance(w, (int, float))
+    assert abs(w-6) < 1e-12
--- vector.py
+++ vector.py
@@ -10,18 +10,29 @@ class Vector3D:
     def __repr__(self):
         return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z}"
 
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
     def __add__(self, other):
-        "string"
-        w = Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
-        return w
+        if isinstance(other, (int, float)):
+            return Vector3D(self.x+other, self.y+other, self.z+other)
+        elif isinstance(other, Vector3D):
+            w = Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
+            return w
+        else:
+            raise TypeError(f"Cannot add Vector3D to object of type {type(other)}")
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y and self.z == other.z
 
+    def dot(self, other):
+        return self.x*other.x + self.y*other.y + self.z*other.z
 
 if __name__ == "__main__":
     v=Vector3D(1, 4, 2)
-    u = Vector3D(1, 1, 1)
-    w=v+u
-    w == Vector3D(2, 5, 4)
+    v.dot(1)
+    #u = Vector3D(1, 1, 1)
+    #w=v+u
+    #w == Vector3D(2, 5, 4)
     
