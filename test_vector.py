c020c1caf26b4879b81f1a0c7d92204a96eab0f0
Vilde Schulerud Bøe <vildesbo@uio.no>
2020-09-04 09:19:41 +0200

    Implement cross and dot product

--- test_vector.py
+++ test_vector.py
@@ -1,8 +1,10 @@
 from vector import Vector3D
 
+
 def test_vector_str():
     v = Vector3D(1, 2, 3)
     assert str(v) == "(1, 2, 3"
+
 
 def test_vector_repr():
     v = Vector3D(1, 2, 3)
@@ -12,25 +14,62 @@ def test_vector_repr():
 def test_vector_add():
     v = Vector3D(1, 2, 3)
     u = Vector3D(1, 1, 1)
-    w = u+v
+    w = u + v
     assert w == Vector3D(2, 3, 4)
+
 
 def test_vector_add_integer_right():
     v = Vector3D(1, 2, 3)
     u = 1
-    w = u+v
+    w = u + v
     assert w == Vector3D(2, 3, 4)
 
+
 import pytest
+
+
 def test_vector_add_str_raises_TypeError():
     v = Vector3D(1, 2, 3)
     u = "Hello"
     with pytest.raises(TypeError):
         u + v
 
+
 def test_vector_dot_product():
     v = Vector3D(1, 2, 3)
     u = Vector3D(1, 1, 1)
     w = u.dot(v)
     assert isinstance(w, (int, float))
-    assert abs(w-6) < 1e-12
+    assert abs(w - 6) < 1e-12
+
+
+def test_vector_dot_produc_mul():
+    v = Vector3D(1, 2, 3)
+    u = Vector3D(1, 1, 1)
+    w = u * v
+    assert isinstance(w, (int, float))
+    assert abs(w - 6) < 1e-12
+
+
+@pytest.mark.parametrize(
+    "u, v, exp",
+    [
+        (Vector3D(0, 1, 0), Vector3D(1, 0, 0), Vector3D(0, 0, -1)),
+        (Vector3D(2, 0, -2), Vector3D(2, 4, 2), Vector3D(8, -8, 8)),
+    ],
+)
+def test_vector_cross_product(u, v, exp):
+    w = u.cross(v)
+    assert w == exp
+
+
+@pytest.mark.parametrize(
+    "u, v, exp",
+    [
+        (Vector3D(0, 1, 0), Vector3D(1, 0, 0), Vector3D(0, 0, -1)),
+        (Vector3D(2, 0, -2), Vector3D(2, 4, 2), Vector3D(8, -8, 8)),
+    ],
+)
+def test_vector_cross_product_matmul(u, v, exp):
+    w = u @ v
+    assert w == exp
--- vector.py
+++ vector.py
@@ -1,8 +1,8 @@
 class Vector3D:
     def __init__(self, x, y, z):
-        self.x=x
-        self.y=y
-        self.z=z
+        self.x = x
+        self.y = y
+        self.z = z
 
     def __str__(self):
         return f"({self.x}, {self.y}, {self.z}"
@@ -10,13 +10,12 @@ class Vector3D:
     def __repr__(self):
         return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z}"
 
-
     def __radd__(self, other):
         return self.__add__(other)
 
     def __add__(self, other):
         if isinstance(other, (int, float)):
-            return Vector3D(self.x+other, self.y+other, self.z+other)
+            return Vector3D(self.x + other, self.y + other, self.z + other)
         elif isinstance(other, Vector3D):
             w = Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
             return w
@@ -27,12 +26,57 @@ class Vector3D:
         return self.x == other.x and self.y == other.y and self.z == other.z
 
     def dot(self, other):
-        return self.x*other.x + self.y*other.y + self.z*other.z
+        r"""
+        Take the dot product between this vector and
+        another vector
+
+        .. math:
+
+            u \cdot v = u_1 v_1 + u_2 + v_2 + u_3 + v_3
+
+        Parameters
+        ------------
+        other : Vector3D
+            The vector to be dotted with
+
+        Returns
+        ------------
+        float
+            The dot product
+        """
+        return self.x * other.x + self.y * other.y + self.z * other.z
+
+    def __mul__(self, other):
+        return self.dot(other)
+
+    def cross(self, other):
+        """
+        Take the cross product between this vector (self)
+        and the other vector
+
+        Parameters
+        -----------
+        other: Vector3D
+            The vector to be crossed with
+
+        Returns
+        -----------
+        Vector3D
+            The cross product
+        """
+        return Vector3D(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x,
+        )
+
+    def __matmul__(self, other):
+        return self.cross(other)
+
 
 if __name__ == "__main__":
-    v=Vector3D(1, 4, 2)
-    v.dot(1)
-    #u = Vector3D(1, 1, 1)
-    #w=v+u
-    #w == Vector3D(2, 5, 4)
-    
+    v = Vector3D(1, 4, 2)
+    # v.dot(1)
+    # u = Vector3D(1, 1, 1)
+    # w=v+u
+    # w == Vector3D(2, 5, 4)
